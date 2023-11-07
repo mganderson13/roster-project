851a092cc3dab7a7801d78cd5935e1572a5acb28
Timothy Chang <123012347+Proton2050@users.noreply.github.com>
2023-11-07 12:17:50 -0800

    add student slice

--- src/client/store/api.js
+++ src/client/store/api.js
@@ -7,14 +7,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
  */
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "/api",
-    prepareHeaders: (headers, { getState }) => {
-      const token = getState().auth.token;
-      if (token) {
-        headers.set("authorization", `Bearer ${token}`);
-      }
-      return headers;
-    },
+    baseUrl: "https://localhost:3000/api",
   }),
   endpoints: () => ({}),
 });

6b73fb0f815347af9f48f3b94e027194ecc23b8e
Timothy Chang <123012347+Proton2050@users.noreply.github.com>
2023-11-07 12:26:05 -0800

    updated navbar

--- src/client/layout/Navbar.jsx
+++ src/client/layout/Navbar.jsx
@@ -1,6 +1,5 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { NavLink, useNavigate } from "react-router-dom";
-import { logout, selectToken } from "../features/auth/authSlice";
 
 import "./Navbar.less";
 
@@ -12,29 +11,13 @@ export default function Navbar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const token = useSelector(selectToken);
-
-  const handleLogout = async () => {
-    await dispatch(logout());
-    navigate("/");
-  };
-
   return (
     <nav className="top">
-      <h1>Task Tracker</h1>
+      <h1>Student Roster</h1>
       <menu>
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
-        {token ? (
-          <li>
-            <a onClick={handleLogout}>Log Out</a>
-          </li>
-        ) : (
-          <li>
-            <NavLink to="/login">Log In</NavLink>
-          </li>
-        )}
       </menu>
     </nav>
   );

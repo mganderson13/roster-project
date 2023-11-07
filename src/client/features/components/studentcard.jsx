e1aa6c40cc4147b640ce90482bec471fe6b73604
yc530 <yoonachoi530@gmail.com>
2023-11-07 14:56:17 -0500

    created student card with data

--- src/client/features/components/studentcard.jsx
+++ src/client/features/components/studentcard.jsx
@@ -6,6 +6,10 @@ return (
         <section className="student-info">
             <h2 className="student-name">{student.firstName} {student.lastName} </h2>
         </section>
+        <img className="student-image" src={student.imageUrl} />
+        <h3>{student.email}</h3>
+        <h4>{student.gpa}</h4>
+        <Link className="student-info" to={`students/${student.id}`}> Student Details </Link>
     </div>
-)
+);
 }

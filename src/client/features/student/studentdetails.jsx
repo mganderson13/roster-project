2fe3a0c738b8d4886b3211c1844ae0b251e3d08c
Jesse Vasquez <jr90box@gmail.com>
2023-11-08 09:45:18 -0800

    data error in student details

--- src/client/features/student/studentdetails.jsx
+++ src/client/features/student/studentdetails.jsx
@@ -1,3 +1,30 @@
+import { useParams } from "react-router-dom";
 import { useGetStudentQuery } from "./studentSlice";
 
-export default function studentDetails()
+export default function StudentDetails() {
+  const { id } = useParams();
+  const { data: students } = useGetStudentQuery();
+
+  const numId = +id;
+  console.log(numId);
+  console.log(typeof numId);
+  //console.log("data", data);
+  console.log("students", students);
+
+  const student = students?.find((student) => student.id === numId);
+
+  if (!student) {
+    return <p>Student not found</p>;
+  }
+
+  return (
+    <div>
+      <h2>
+        {student.firstName} {student.lastName}
+      </h2>
+      <p>Email: {student.email}</p>
+      <img src={student.imageUrl} />
+      <p>Gpa: {student.gpa}</p>
+    </div>
+  );
+}
